abf58c6047d58189c58c48d6cac8790b128de593
AsimHayran <168582628+AsimHayran@users.noreply.github.com>
2024-06-19 19:09:11 +0100

    Update Asim.js

--- Asim.js
+++ Asim.js
@@ -1,48 +1,16 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const fileLinks = document.querySelectorAll('.file-link');
-    const progressBar = document.getElementById('progress-bar');
+function clearDisplay() {
+    document.getElementById('display').value = '';
+}
 
-    fileLinks.forEach(function(link) {
-        link.addEventListener('click', function(event) {
-            event.preventDefault(); // Prevent default link behavior (navigation)
-            const fileUrl = this.getAttribute('data-url');
-            downloadFile(fileUrl);
-        });
-    });
+function inputValue(value) {
+    document.getElementById('display').value += value;
+}
 
-    function downloadFile(url) {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url, true);
-        xhr.responseType = 'blob';
-
-        xhr.onload = function() {
-            if (xhr.status === 200) {
-                const blob = xhr.response;
-                const url = URL.createObjectURL(blob);
-                const a = document.createElement('a');
-                a.style.display = 'none';
-                a.href = url;
-                a.download = getFileNameFromUrl(url);
-                document.body.appendChild(a);
-                a.click();
-                window.URL.revokeObjectURL(url);
-                document.body.removeChild(a); // Clean up
-            }
-        };
-
-        xhr.addEventListener('progress', function(e) {
-            if (e.lengthComputable) {
-                const percentComplete = (e.loaded / e.total) * 100;
-                progressBar.style.width = percentComplete + '%';
-            }
-        });
-
-        xhr.send();
+function calculate() {
+    try {
+        const result = eval(document.getElementById('display').value);
+        document.getElementById('display').value = result;
+    } catch {
+        document.getElementById('display').value = 'Error';
     }
-
-    function getFileNameFromUrl(url) {
-        const startIndex = url.lastIndexOf('/') + 1;
-        const endIndex = url.lastIndexOf('?') !== -1 ? url.lastIndexOf('?') : url.length;
-        return decodeURIComponent(url.substring(startIndex, endIndex));
-    }
-});
+}
